10a36a478f69289b47bd0ab004b3448beda46d9f
lgants <lgants@gmail.com>
2016-11-17 13:13:44 -0600

    completed heap lab

--- lib/algorithms_heap.rb
+++ lib/algorithms_heap.rb
@@ -1,1 +1,27 @@
 # code your solution here
+class MinHeap
+  attr_accessor :data, :root
+
+  def initialize
+    @data = []
+    @root = nil
+  end
+
+  def push(n)
+    if @root == nil
+      @root = n
+    elsif n < @root
+      @root = n
+    end
+    @data.push(n)
+  end
+
+  def pop
+    if @data.last == @root
+      x = @data.pop
+      @root = @data.min
+    end
+    x ||= @data.pop
+  end
+
+end
